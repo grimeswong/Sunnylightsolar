b67d7c7a6408bbf541c4e3512c4b259c94f4a550
Shadow Grimes <grimeswongatwork@gmail.com>
2017-11-22 14:44:08 +0000

    deleted lines of unused query functions

--- productQueryDb.php
+++ productQueryDb.php
@@ -8,33 +8,6 @@
       }
    }
 
-   function getCustomers($searchTerm = null) {
-
-      $db = new MyDB();
-      if(!$db){
-         echo '<script type="text/javascript">alert("'.$db->lastErrorMsg().'");</script>';
-      } else {
-         //echo "Opened database successfully\n";
-      }
-      if(!$searchTerm) {
-         $sql ='SELECT * from CUSTOMERS;';
-      } else {
-         $sql ='SELECT * FROM CUSTOMERS WHERE FIRSTNAME LIKE "'.$searchTerm.'" OR LASTNAME LIKE "'.$searchTerm.'" OR ADDRESS LIKE "'.$searchTerm.'" OR PHONE  LIKE "'.$searchTerm.'"';
-      }
-      $ret = $db->query($sql);
-      if(!$ret){
-        echo $db->lastErrorMsg();
-        return [];
-      } else {
-         while($row = $ret->fetchArray(SQLITE3_ASSOC) ){
-            $array[] = $row;
-         }
-         $db->close();
-         return $array;
-      }
-   }
-
-
    function getPanels($searchTerm = null) {
 
       $db = new MyDB();
@@ -90,49 +63,5 @@
          return $array;
       }
    }
-
-
-
-  //  function addCustomer($fname, $lname, $address, $phone) {
-   //
-  //     $db = new MyDB();
-  //     if(!$db){
-  //        echo '<script type="text/javascript">alert("'.$db->lastErrorMsg().'");</script>';
-  //     } else {
-  //        //echo "Opened database successfully\n";
-  //     }
-   //
-  //     $sql ='INSERT INTO CUSTOMERS (FIRSTNAME, LASTNAME, ADDRESS, PHONE) VALUES ("'.$fname.'", "'.$lname.'", "'.$address.'", "'.$phone.'");';
-  //     $db->query($sql);
-  //  }
-   //
-   //
-  //  function addProduct($pname, $man, $desc, $price) {
-   //
-  //     $db = new MyDB();
-  //     if(!$db){
-  //        echo '<script type="text/javascript">alert("'.$db->lastErrorMsg().'");</script>';
-  //     } else {
-  //        //echo "Opened database successfully\n";
-  //     }
-   //
-  //     $sql ='INSERT INTO PRODUCTS (PRODUCTNAME, MANUFACTURER, DESCRIPTION, PRICE) VALUES ("'.$pname.'", "'.$man.'", "'.$desc.'", "'.$price.'");';
-  //     $db->query($sql);
-  //  }
-   //
-   //
-  //  function addEvent($ename, $loc, $desc, $date) {
-   //
-  //     $db = new MyDB();
-  //     if(!$db){
-  //        echo '<script type="text/javascript">alert("'.$db->lastErrorMsg().'");</script>';
-  //     } else {
-  //        //echo "Opened database successfully\n";
-  //     }
-   //
-  //     $sql ='INSERT INTO EVENTS (EVENTNAME, LOCATION, DESCRIPTION, DATE) VALUES ("'.$ename.'", "'.$loc.'", "'.$desc.'", "'.$date.'");';
-  //     $db->query($sql);
-  //  }
-
 
 ?>
